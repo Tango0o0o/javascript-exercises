2a97e60a877bc7c7c54880618c10e45713ca676b
Tango0o0o <dacostawalanyo49@gmail.com>
2024-11-17 18:33:46 +0000

    Completed exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    reversedstring = ''
 
+
+    for (let i = string.length-1; i > -1; i--) {
+        reversedstring += string[i]  
+    };
+
+    return reversedstring
 };
 
 // Do not edit below this line
