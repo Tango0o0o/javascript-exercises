65cd94b0b8310b4afce7f468a09dcc895e214bfa
Tango0o0o <dacostawalanyo49@gmail.com>
2024-11-17 18:23:27 +0000

    Completed exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    repeated_string = '';
+    if (num < 0) {
+        return 'ERROR'
+    };
+    for (let i = 0; i < num; i++) {
+        repeated_string += string
+    };
+    return repeated_string;
 };
 
 // Do not edit below this line
