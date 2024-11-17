3d6ea9537d312fee18a720b27204cdf6f121bb2e
Tango0o0o <dacostawalanyo49@gmail.com>
2024-11-17 18:55:37 +0000

    Done 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+    n = 0
 
+    if (!Number.isInteger(x) || !Number.isInteger(y)) return "ERROR";
+    if (x < 0 || y < 0) return "ERROR";
+
+    if (x < y) {
+        for (let i = x; i < y+1; i++) {
+            n+=i
+            
+        }
+    } else {
+        for (let i = y; i < x+1; i++) {
+            n+=i
+            
+        }
+    }
+     
+
+    return n
 };
 
 // Do not edit below this line
