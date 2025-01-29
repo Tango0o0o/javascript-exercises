bfbbe19193c389d9bc3f0174d8b7d8dc3f616993
Tango0o0o <dacostawalanyo49@gmail.com>
2025-01-29 21:19:10 +0000

    Completed exercise 06

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,23 +1,21 @@
-const sumAll = function(x, y) {
-    n = y
+const sumAll = function(x, y) { // x to be smaller y to be bigger
+    let temp = 0
 
     if (!Number.isInteger(x) || !Number.isInteger(y)) return "ERROR";
     if (x < 0 || y < 0) return "ERROR";
 
-    if (x > y) {
-        while (n > y) {
-            n += x
-            x-=1
-        }
-    } else {
-        while (n > x) {
-            n += y
-            x-=1
-        }
-    }
+    if (y > x) {
+        temp = x
+        x = y
+        y = temp
+        temp = 0
+    };
      
+    for (let index = y; index <= x; index++) {
+        temp += index
+    }
 
-    return n
+    return temp
 };
 
 // Do not edit below this line
